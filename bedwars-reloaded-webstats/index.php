3de1852b008233b55d6e13b928c0b3e40b6e5893
Yannici <yannicschne@gmail.com>
2015-06-30 19:05:12 +0200

    Added scale to index (example)

--- bedwars-reloaded-webstats/index.php
+++ bedwars-reloaded-webstats/index.php
@@ -23,6 +23,7 @@ $webstats = new BedwarsWebstats()
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 		<title>Bedwars-Webstats :: Example</title>
 		<?php $webstats->displayCss(); ?>
 	</head>
